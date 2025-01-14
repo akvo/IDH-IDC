7534ae232fba15c0ea873538bf1ca7164742aac1
wayangalihpratama <wgprtm@gmail.com>
2025-01-14 16:46:21 +0800

    [#389] Handle case button on AssessImpactMitigationStrategies

--- frontend/src/pages/cases/steps/AssessImpactMitigationStrategies.js
+++ frontend/src/pages/cases/steps/AssessImpactMitigationStrategies.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { stepPath, CurrentCaseState } from "../store";
 
-const AssessImpactMitigationStrategies = () => {
+/**
+ * STEP 4
+ */
+const AssessImpactMitigationStrategies = ({
+  setbackfunction,
+  setnextfunction,
+}) => {
+  const navigate = useNavigate();
+  const currentCase = CurrentCaseState.useState((s) => s);
+
+  const backFunction = useCallback(() => {
+    navigate(`/case/${currentCase.id}/${stepPath.step3.label}`);
+  }, [navigate, currentCase.id]);
+
+  const nextFunction = useCallback(() => {
+    navigate(`/case/${currentCase.id}/${stepPath.step5.label}`);
+  }, [navigate, currentCase.id]);
+
+  useEffect(() => {
+    if (setbackfunction) {
+      setbackfunction(backFunction);
+    }
+    if (setnextfunction) {
+      setnextfunction(nextFunction);
+    }
+  }, [setbackfunction, setnextfunction, backFunction, nextFunction]);
+
   return <div>AssessImpactMitigationStrategies</div>;
 };
 
