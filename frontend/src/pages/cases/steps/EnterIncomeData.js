c90a0b7eb4065168f1d2103b296d4b9214be86c5
wayangalihpratama <wgprtm@gmail.com>
2025-01-14 17:47:34 +0800

    [#389] Get commodity questions

--- frontend/src/pages/cases/steps/EnterIncomeData.js
+++ frontend/src/pages/cases/steps/EnterIncomeData.js
@@ -1,6 +1,9 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { stepPath, CurrentCaseState } from "../store";
+import { api } from "../../../lib";
+
+const commodityOrder = ["focus", "secondary", "tertiary", "diversified"];
 
 /**
  * STEP 2
@@ -8,6 +11,7 @@ import { stepPath, CurrentCaseState } from "../store";
 const EnterIncomeData = ({ segment, setbackfunction, setnextfunction }) => {
   const navigate = useNavigate();
   const currentCase = CurrentCaseState.useState((s) => s);
+  const [questionGroups, setQuestionGroups] = useState([]);
 
   const backFunction = useCallback(() => {
     navigate(`/case/${currentCase.id}/${stepPath.step1.label}`);
@@ -25,6 +29,29 @@ const EnterIncomeData = ({ segment, setbackfunction, setnextfunction }) => {
       setnextfunction(nextFunction);
     }
   }, [setbackfunction, setnextfunction, backFunction, nextFunction]);
+
+  // Fetch questions for income data entry
+  useEffect(() => {
+    if (currentCase?.id && currentCase?.case_commodities?.length) {
+      const reorderedCaseCommodities = commodityOrder
+        .map((co) => {
+          const findCommodity = currentCase.case_commodities.find(
+            (cc) => cc.commodity_type === co
+          );
+          return findCommodity;
+        })
+        .filter((x) => x);
+
+      api.get(`/questions/${currentCase.id}`).then((res) => {
+        const { data } = res;
+        const dataTmp = reorderedCaseCommodities
+          .map((cc) => data.find((d) => d.commodity_id === cc.commodity))
+          .filter((x) => x);
+        setQuestionGroups(dataTmp);
+      });
+    }
+  }, [currentCase.id, currentCase.case_commodities]);
+  console.log(questionGroups);
 
   return <div>EnterIncomeData {segment.name}</div>;
 };
