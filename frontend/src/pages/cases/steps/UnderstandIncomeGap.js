21ea36004868e84f50087ff260e9afc9cb6250fd
wayangalihpratama <wgprtm@gmail.com>
2025-01-24 15:39:11 +0800

    [#399] Enable show label and export btn in Understand income gap page

--- frontend/src/pages/cases/steps/UnderstandIncomeGap.js
+++ frontend/src/pages/cases/steps/UnderstandIncomeGap.js
@@ -37,12 +37,13 @@ const UnderstandIncomeGap = ({ setbackfunction, setnextfunction }) => {
     <Row id="understand-income-gap" gutter={[24, 24]}>
       <Col span={24} className="header-wrapper">
         <Space direction="vertical">
-          <div className="title">Explanatory text</div>
+          <div className="title">What is the current income situation?</div>
           <div className="description">
-            This page enables you to explore various scenarios by adjusting your
-            income drivers in different ways across your segments. This allows
-            you to understand the potential paths towards improving farmer
-            household income
+            This page provides an overview of the current income situation of
+            farmers across various segments, offering insights into the living
+            income gap. It also enables a detailed exploration of the
+            composition of income drivers, helping you better understand their
+            contributions.
           </div>
         </Space>
       </Col>
@@ -58,6 +59,13 @@ const UnderstandIncomeGap = ({ setbackfunction, setnextfunction }) => {
       <Col span={24}>
         <CompareIncomeGap />
       </Col>
+      {/* EOL Chart */}
+      <Col span={24}>
+        <Card className="card-section-wrapper">
+          Understand current income and the income gap
+        </Card>
+      </Col>
+      {/* Chart */}
       <Col span={24}>
         <ChartExploreIncomeDriverBreakdown />
       </Col>
