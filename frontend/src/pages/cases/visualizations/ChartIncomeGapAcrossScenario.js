0199b952c9812cd63f0b6b453a2a421b082840e5
wayangalihpratama <wgprtm@gmail.com>
2025-02-18 11:35:26 +0800

    [#398] Finalize Income gap accross scenario chart

--- frontend/src/pages/cases/visualizations/ChartIncomeGapAcrossScenario.js
+++ frontend/src/pages/cases/visualizations/ChartIncomeGapAcrossScenario.js
@@ -1,14 +1,88 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useRef } from "react";
 import { Card, Row, Col, Space, Select } from "antd";
 import { VisualCardWrapper } from "../components";
 import { selectProps } from "../../../lib";
 import { CaseVisualState, CurrentCaseState } from "../store";
 import { orderBy } from "lodash";
+import Chart from "../../../components/chart";
+
+const generateTargetChartData = (data) => {
+  const target = [
+    {
+      name: "Income Target",
+      type: "line",
+      symbol: "diamond",
+      symbolSize: 15,
+      color: "#000",
+      lineStyle: {
+        width: 0,
+      },
+      data: data.map((d) => ({
+        name: "Benchmark",
+        value: d?.target ? Math.round(d.target) : 0,
+      })),
+    },
+  ];
+  return target;
+};
+
+const generateChartData = (data, current = false) => {
+  return data.map((d) => {
+    const incomeTarget = d?.currentSegmentValue?.target || 0;
+    const currentTotalIncome =
+      d?.currentSegmentValue?.total_current_income || 0;
+
+    const newTotalIncome =
+      d?.updatedSegmentScenarioValue?.total_current_income || 0;
+    const additionalValue = newTotalIncome
+      ? newTotalIncome - currentTotalIncome
+      : 0;
+
+    let gapValue = incomeTarget - newTotalIncome;
+    gapValue = gapValue < 0 ? 0 : gapValue;
+
+    return {
+      name: current ? d.name : `${d.scenarioName}-${d.name}`,
+      target: Math.round(incomeTarget),
+      stack: [
+        {
+          name: "Current total\nhousehold income",
+          title: "Current total\nhousehold income",
+          value: Math.round(currentTotalIncome),
+          total: Math.round(currentTotalIncome),
+          color: "#1B625F",
+          order: 1,
+        },
+        {
+          name: "Additional income\nwhen income drivers\nare changed",
+          title: "Additional income\nwhen income drivers\nare changed",
+          value: Math.round(additionalValue),
+          total: Math.round(additionalValue),
+          color: "#49D985",
+          order: 2,
+        },
+        {
+          name: "Gap",
+          title: "Gap",
+          value: Math.round(gapValue),
+          total: Math.round(gapValue),
+          color: "#F9CB21",
+          order: 3,
+        },
+      ],
+    };
+  });
+};
 
 const ChartIncomeGapAcrossScenario = () => {
   const currentCase = CurrentCaseState.useState((s) => s);
   const { scenarioModeling } = CaseVisualState.useState((s) => s);
   const scenarioData = scenarioModeling.config.scenarioData;
+
+  const [selectedScenarioSegmentChart, setSelectedScenarioSegmentChart] =
+    useState([]);
+  const [showLabel, setShowLabel] = useState(false);
+  const chartRef = useRef(null);
 
   const scenarioSegmentOptions = useMemo(() => {
     let i = 1;
@@ -27,12 +101,54 @@ const ChartIncomeGapAcrossScenario = () => {
     return res;
   }, [scenarioData, currentCase.segments]);
 
+  const chartData = useMemo(() => {
+    const scenarioValues = scenarioData
+      .flatMap((sd) => {
+        return sd.scenarioValues.map((sv) => ({
+          scenarioSegmentKey: `${sd.key}-${sv.segmentId}`,
+          scenarioName: sd.name,
+          ...sv,
+        }));
+      })
+      .filter((sv) => {
+        if (selectedScenarioSegmentChart.length) {
+          return selectedScenarioSegmentChart.includes(sv.scenarioSegmentKey);
+        }
+        return sv;
+      });
+    return generateChartData(scenarioValues);
+  }, [scenarioData, selectedScenarioSegmentChart]);
+
+  const targetChartData = useMemo(
+    () => generateTargetChartData(chartData),
+    [chartData]
+  );
+
   return (
     <Card className="card-visual-wrapper">
       <Row gutter={[20, 20]} align="top">
         <Col span={16}>
-          <VisualCardWrapper title="Income gap across scenario" bordered>
-            Chart
+          <VisualCardWrapper
+            title="Income gap across scenario"
+            bordered
+            showLabel={showLabel}
+            setShowLabel={setShowLabel}
+            exportElementRef={chartRef}
+            exportFilename="What are the results for the different segments across scenarios?"
+          >
+            <Chart
+              wrapper={false}
+              type="BARSTACK"
+              data={chartData}
+              targetData={targetChartData}
+              loading={!chartData.length}
+              extra={{
+                axisTitle: { y: `Income ${currentCase?.currency || ""}` },
+              }}
+              grid={{ right: 161 }}
+              showLabel={showLabel}
+              height={385}
+            />
           </VisualCardWrapper>
         </Col>
         <Col span={8}>
@@ -52,7 +168,7 @@ const ChartIncomeGapAcrossScenario = () => {
                 options={scenarioSegmentOptions}
                 placeholder="Select Scenario - Segment"
                 mode="multiple"
-                // onChange={setSelectedScenarioSegmentChart}
+                onChange={setSelectedScenarioSegmentChart}
               />
             </div>
           </Space>
