c528b5375e0e85985a1893fc3d1894210508f80d
wayangalihpratama <wgprtm@gmail.com>
2025-02-28 19:23:27 +0800

    [#424] Initial handle optimization settings

--- frontend/src/pages/cases/components/OptimizeIncomeTarget.js
+++ frontend/src/pages/cases/components/OptimizeIncomeTarget.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Button, Card, Col, InputNumber, Row, Space } from "antd";
 import { ArrowRightOutlined } from "@ant-design/icons";
 import AllDriverTreeSelector from "./AllDriverTreeSelector";
+import { CaseVisualState, CurrentCaseState } from "../store";
+import { thousandFormatter } from "../../../components/chart/options/common";
 
 const OptimizeIncomeTarget = ({ selectedSegment }) => {
+  const currentCase = CurrentCaseState.useState((s) => s);
+  const { dashboardData } = CaseVisualState.useState((s) => s);
+
+  const [increaseValues, setIncreaseValues] = useState({
+    "percentage-increase-1": null,
+    "absolute-increase-1": 0,
+    "percentage-increase-2": null,
+    "absolute-increase-2": 0,
+    "percentage-increase-3": null,
+    "absolute-increase-3": 0,
+  });
+
+  const currency = currentCase?.currency || "";
+
+  const currentDashboardData = useMemo(() => {
+    return dashboardData.find((d) => d.id === selectedSegment);
+  }, [selectedSegment]);
+
+  const handleChangeIncreaseValues = ({ index, percentage }) => {
+    const percentageField = `percentage-increase-${index}`;
+    setIncreaseValues((prev) => ({
+      ...prev,
+      [percentageField]: percentage,
+    }));
+  };
+
   return (
     <Card className="card-section-wrapper optimize-income-target-wrapper">
       <Row gutter={[20, 20]}>
@@ -43,26 +71,69 @@ const OptimizeIncomeTarget = ({ selectedSegment }) => {
           <div className="income-inputs-wrapper">
             <div>
               <label>Current income</label>
-              <InputNumber controls={false} />
+              <InputNumber
+                controls={false}
+                value={thousandFormatter(
+                  currentDashboardData?.total_current_income || 0,
+                  2
+                )}
+                addonAfter={currency}
+                readOnly={true}
+              />
             </div>
             <div>
               <label>Feasible income</label>
-              <InputNumber controls={false} />
+              <InputNumber
+                controls={false}
+                value={thousandFormatter(
+                  currentDashboardData?.total_feasible_income || 0,
+                  2
+                )}
+                addonAfter={currency}
+                readOnly={true}
+              />
             </div>
             <div>
               <label>Increase 1</label>
-              <InputNumber controls={false} addonAfter="%" />
-              <span>increase 1 value</span>
+              <InputNumber
+                controls={false}
+                addonAfter="%"
+                onChange={(value) =>
+                  handleChangeIncreaseValues({ index: 1, percentage: value })
+                }
+                value={increaseValues?.["percentage-increase-1"] || null}
+              />
+              {increaseValues?.["percentage-increase-1"] && (
+                <span>increase 1 {currency}</span>
+              )}
             </div>
             <div>
               <label>Increase 2</label>
-              <InputNumber controls={false} addonAfter="%" />
-              <span>increase 2 value</span>
+              <InputNumber
+                controls={false}
+                addonAfter="%"
+                onChange={(value) =>
+                  handleChangeIncreaseValues({ index: 2, percentage: value })
+                }
+                value={increaseValues?.["percentage-increase-2"] || null}
+              />
+              {increaseValues?.["percentage-increase-2"] && (
+                <span>increase 2 {currency}</span>
+              )}
             </div>
             <div>
               <label>Increase 3</label>
-              <InputNumber controls={false} addonAfter="%" />
-              <span>increase 3 value</span>
+              <InputNumber
+                controls={false}
+                addonAfter="%"
+                onChange={(value) =>
+                  handleChangeIncreaseValues({ index: 3, percentage: value })
+                }
+                value={increaseValues?.["percentage-increase-3"] || null}
+              />
+              {increaseValues?.["percentage-increase-3"] && (
+                <span>increase 3 {currency}</span>
+              )}
             </div>
           </div>
         </Col>
