c1679fe5e5dc886afe3f09ad9e355bb585dfb36d
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-07 16:08:51 +0700

    [#438] Add ClockIcon component to icon library

--- frontend/src/lib/icon.js
+++ frontend/src/lib/icon.js
@@ -517,3 +517,28 @@ export const OvalIcon = () => (
     />
   </svg>
 );
+
+export const ClockIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19Z"
+      stroke="currentColor"
+      strokeOpacity="0.81"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M11.9993 12.0002L9.66602 13.5558M11.9993 8.11133V12.0002"
+      stroke="currentColor"
+      strokeOpacity="0.81"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
