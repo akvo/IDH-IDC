b776fdf0ee442dcd46a05f6e449b9af710f5ee53
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-07 16:57:27 +0700

    [#438] Add ProcurementPage export for Assessment, InterventionLibrary, Methodology, and Practice components

--- frontend/src/pages/procurement-library/index.js
+++ frontend/src/pages/procurement-library/index.js
@@ -1,1 +1,12 @@
+import { Assessment } from "./assessment";
+import { InterventionLibrary } from "./intervention-library";
+import { Methodology } from "./methodology";
+import { Practice } from "./practice";
+
 export { default as ProcurementLibrary } from "./ProcurementLibrary";
+export const ProcurementPage = {
+  Assessment,
+  InterventionLibrary,
+  Methodology,
+  Practice,
+};
