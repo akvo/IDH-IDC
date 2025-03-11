197e3b4992d92f99034ae13b001498613948609c
wayangalihpratama <wgprtm@gmail.com>
2025-03-11 12:04:32 +0800

    [#448] Create cii jumbotron page

--- frontend/src/pages/cocoa-income-inventory/CocoaIncomeInventory.js
+++ frontend/src/pages/cocoa-income-inventory/CocoaIncomeInventory.js
@@ -1,7 +1,54 @@
 import React from "react";
+import "./cii.scss";
+import { Link } from "react-router-dom";
+import { Row, Col } from "antd";
+import { LandingIDHLogo } from "../../lib/icon";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 const CocoaIncomeInventory = () => {
-  return <div>CocoaIncomeInventory</div>;
+  return (
+    <Row id="cii-page" gutter={[24, 24]}>
+      {/* Jumbotron */}
+      <Col span={24}>
+        <div className="jumbotron-wrapper">
+          <LandingIDHLogo
+            style={{
+              position: "absolute",
+              bottom: -180,
+              right: -165,
+              transform: "rotate(260deg)",
+            }}
+            width={400}
+            height={400}
+          />
+          <div className="jumbotron-gradient-wrapper">
+            <div className="jumbotron-gradient-overlay"></div>
+            <div className="jumbotron-text-wrapper">
+              <h1>
+                Welcome to the
+                <br />
+                cocoa income inventory
+              </h1>
+              <h3>
+                Explore data on cocoa production, income from cocoa and other
+                key indicators from different origins
+              </h3>
+              <Link to="/" className="button button-yellow">
+                Explore dashboard{" "}
+                <ArrowRightOutlined style={{ fontSize: 12, fontWeight: 900 }} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Col>
+
+      {/* About */}
+      <Col span={24}>
+        <div className="logos-wrapper"></div>
+        <div className="about-content-wrapper"></div>
+      </Col>
+    </Row>
+  );
 };
 
 export default CocoaIncomeInventory;
