227914918b079999baf3acba7854d38c8fb924c5
wayangalihpratama <wgprtm@gmail.com>
2025-03-20 20:25:18 +0800

    [#477] Update about cii cols

--- frontend/src/pages/cocoa-income-inventory/IndicatorCarousel.js
+++ frontend/src/pages/cocoa-income-inventory/IndicatorCarousel.js
@@ -23,14 +23,21 @@ const IndicatorCarousel = () => {
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1080,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 700,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
