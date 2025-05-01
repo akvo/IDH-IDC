e30baf2383ad5452c1b0f34a4de581f5e0c69eec
wayangalihpratama <wgprtm@gmail.com>
2025-05-01 18:28:42 +0800

    [#578] Update standardising

--- frontend/src/pages/cocoa-income-inventory/static/contributing-content.js
+++ frontend/src/pages/cocoa-income-inventory/static/contributing-content.js
@@ -1,6 +1,6 @@
 export const contributingContent = [
   "The CIl establishes a structured approach for sharing aggregated cocoa sector data in an anonymised and pre-competitive manner, ensuring that insights are accessible without compromising commercial interests.",
-  "By standardizing data-sharing practices, ClI fosters a culture of transparency and evidence-based decision-making, benefiting all sector stakeholders.",
+  "By standardising data-sharing practices, ClI fosters a culture of transparency and evidence-based decision-making, benefiting all sector stakeholders.",
   "The Cocoa Household Income Study Approach (CHIS) is already shaping industry-wide standards for income measurement. CHIS and CIl work in synergy, with CHIS providing a rigorous, standardised methodology for data collection and CII ensuring this data is shared, harmonised, and leveraged to drive sector-wide improvements.",
   "It provides a common platform where companies, NGOs, policymakers, producer organisations, farmers and researchers can access validated data, reducing duplication of research efforts and promoting efficiency and learning.",
   "The Cocoa Household Income Study Approach (CHIS) is already shaping industry-wide standards for income measurement.",
