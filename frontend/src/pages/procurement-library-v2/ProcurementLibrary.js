81cae3aeeae645b343ea53d7a9402d912b64f22e
wayangalihpratama <wgprtm@gmail.com>
2025-10-28 10:29:53 +0800

    [#640] Add procurement principles img & update sourcing cycle section text position

--- frontend/src/pages/procurement-library-v2/ProcurementLibrary.js
+++ frontend/src/pages/procurement-library-v2/ProcurementLibrary.js
@@ -20,7 +20,7 @@ import { useWindowDimensions } from "../../hooks";
 import { OtherToolsAndResources } from "../../components/utils";
 import SustainableProcurementImage from "../../assets/images/procurement-library/sustainable-procurement.png";
 import SourcingStrategyCycleImage from "../../assets/images/procurement-library/sourcing-strategy-cycle.png";
-import VideoPlayerImage from "../../assets/images/procurement-library/video-player.png";
+import ProcurementPrinciplesCurves from "../../assets/images/procurement-library/principles-for-procurement-curves.svg";
 import CheckCircleIcon from "../../assets/icons/procurement-library/check-circle.png";
 import TCOChartImage from "../../assets/images/procurement-library/total-cost-of-ownership-chart.png";
 
@@ -246,10 +246,6 @@ const ProcurementLibrary = () => {
               each step of the four stages of a typical Sourcing Strategy Cycle
               approach - whether a strategy is being created or updated.
             </p>
-            <p>
-              Explore each stage of the Cycle and the outline opportunities for
-              integrating more sustainable practices.
-            </p>
           </div>
           <div className="sscc-title-button">
             <Link
@@ -264,6 +260,11 @@ const ProcurementLibrary = () => {
         {/* body */}
         <div className="sscc-body-wrapper">
           <div className="sscc-body-left">
+            <p>
+              Explore each stage of the Cycle and the outline opportunities for
+              integrating more sustainable practices.
+            </p>
+
             {/* tabs */}
             <SourcingStrategyCycleTabs
               sourcingStrategyCycleTab={sourcingStrategyCycleTab}
@@ -344,7 +345,12 @@ const ProcurementLibrary = () => {
           </div>
         </div>
         <div className="spp-image-wrapper">
-          <img src={VideoPlayerImage} alt="video-player-thumbnail" />
+          <div className="spp-image-border">
+            <img
+              src={ProcurementPrinciplesCurves}
+              alt="procurement-principles-curves-img"
+            />
+          </div>
         </div>
       </div>
       {/* EOL Sustainable Procurement Principles */}
