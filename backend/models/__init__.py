b53d6e3160d2d09da8f17fbc3dcb00c07fc4b8db
wayangalihpratama <wgprtm@gmail.com>
2025-12-23 18:49:12 +0800

    [#672] Add public_key to question driver

---
+++ backend/models/__init__.py
@@ -0,0 +1,10 @@
+# IMPORTANT: import all models here
+from models.user import User  # noqa
+from models.question import Question  # noqa
+from models.company import Company  # noqa
+from models.commodity_category import CommodityCategory  # noqa
+from models.commodity_category_question import (  # noqa
+    CommodityCategoryQuestion,
+)
+
+# add all other models
