95dd38fa546a94e119d236915510e2ecaaefe856
wayangalihpratama <wgprtm@gmail.com>
2026-02-05 11:27:52 +0800

    [#709] Fix frontend lint: remove unused useState import in ThreeDriverCalculator.js

--- frontend/src/pages/cases/components/ThreeDriverCalculator.js
+++ frontend/src/pages/cases/components/ThreeDriverCalculator.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { Card, Space, Row, Col, Select, Tooltip } from "antd";
 import { InfoCircleOutlined } from "@ant-design/icons";
 import { CaseVisualState, CurrentCaseState } from "../store";
